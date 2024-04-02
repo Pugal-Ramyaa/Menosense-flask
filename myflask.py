0ec5db987275337fb21e1b851cc214d2c6e5cac0
PugalRamyaa <pugalramyaa.p2020@vitstudent.ac.in>
2024-04-02 13:52:43 +0530

    second commit

--- myflask.py
+++ myflask.py
@@ -7,15 +7,15 @@ import pandas as pd
 app = Flask(__name__)
 
 # Load the trained models
-rf_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/rf_model.joblib')
-gb_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/gb_model.joblib')
-ada_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/ada_model.joblib')
-xgb_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/xgb_model.joblib')
-mlp_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/mlp_model.joblib')
-extra_trees_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/et_model.joblib')
-knn_best_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/knn_model.joblib')
-svc_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/svc_model.joblib')
-meta_model = joblib.load('C:/Users/Ramya/Desktop/New folder/tflite/meta_model.joblib')
+rf_best_model = joblib.load('./rf_model.joblib')
+gb_best_model = joblib.load('./gb_model.joblib')
+ada_best_model = joblib.load('./ada_model.joblib')
+xgb_best_model = joblib.load('./xgb_model.joblib')
+mlp_best_model = joblib.load('./mlp_model.joblib')
+extra_trees_best_model = joblib.load('./et_model.joblib')
+knn_best_model = joblib.load('./knn_model.joblib')
+svc_model = joblib.load('./svc_model.joblib')
+meta_model = joblib.load('./meta_model.joblib')
 
 # Load the recommendation models
 recommendation_models = {}
